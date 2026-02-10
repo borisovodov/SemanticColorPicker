85ec2cc017a27ef129e00f0308f6d7538d362b05
Boris Ovodov <boris@ovodov.me>
2026-02-10 12:20:17 +0500

    Transfer `Codable` dep from protocol to implementation

--- Sources/SemanticColorPicker/ColorConvertible.swift
+++ Sources/SemanticColorPicker/ColorConvertible.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 /// A type with a color representation.
-public protocol ColorConvertible: CustomStringConvertible, Equatable, Codable {
+public protocol ColorConvertible: CustomStringConvertible, Equatable {
     
     /// A color representation of this instance.
     var color: Color { get }
